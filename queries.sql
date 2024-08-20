17edbf7509799acb37a714f4407c4c79c07a2ffa
vlobolochkov <126956125+vlobolochkov@users.noreply.github.com>
2024-08-20 14:16:19 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -100,6 +100,82 @@ from sales_price
 inner join sellers_list on sellers_list.employee_id = sales_price.sales_person_id -- джоин таблицы sellers_list для указания имени продавца
 group by seller, day_of_week -- группировка по имени продавца и дню недели
 order by day_of_week, seller; -- сортировка по дню недели и имени продавца
-
+------------------------------------------------------------------------------------
 6 урок:
 
+1. age_groups: 
+
+select '16-25' as age_category, -- объявление нового столбца со значением категории
+	count(age) as age_count -- подсчет кол-ва клиентов по возрасту
+from customers
+where age between 16 and 25 -- фильтрация клиентов по возрастной категории
+union -- объединение таблиц с категориями
+select '26-40' as age_category, --дополнение столбца с возрастной категорией
+	count(age) as age_count -- подсчет кол-ва клиентов по возрасту
+from customers
+where age between 26 and 40 -- фильтрация клиентов по возрастной категории
+union -- объединение таблиц с категориями
+select '40+' as age_category, --дополнение столбца с возрастной категорией
+	count(age) as age_count -- подсчет кол-ва клиентов по возрасту
+from customers
+where age > 40 -- фильтрация клиентов по возрастной категории
+order by age_category; -- сортируем по возрастной категории
+
+2. customers_by_month:
+
+with sales_price as (
+	select sales_id
+	customer_id,
+	sales.product_id,
+	quantity,
+	products.price, -- подтягиваем цену продукта
+	products.price * quantity as total, -- получаем выручку
+	to_char(sale_date, 'YYYY-MM') as selling_month -- форматируем дату в необходимый формат
+from sales
+inner join products on sales.product_id = products.product_id -- джоин таблицы products для получения цены продукта
+)
+
+select selling_month,
+	count(customer_id) as total_customers, -- подсчет клиентов
+	floor(sum(total)) as income -- суммирование выручки
+from sales_price
+group by selling_month -- группировка по месяцу
+order by selling_month; -- сортировка по месяцу
+
+3. special_offer:
+
+with sales_price as (
+	select sales_id,
+	sales_person_id,
+	customer_id,
+	sales.product_id,
+	price, -- цена продукта
+	quantity,
+	sale_date
+from sales
+inner join products on products.product_id = sales.product_id -- джоин таблицы products для получения цены продукта
+where products.price = 0 -- фильтрация таблицы для получения только акционных покупок
+),
+
+sellers_list as (
+	select employee_id,
+	concat(first_name, ' ', last_name) as seller -- конкатенация имени и фамилии продавца
+	from employees
+),
+
+customers_list as (
+	select customer_id,
+	concat(first_name, ' ', last_name) as customer -- конкатенация имени и фамилии покупателя
+	from customers
+)
+
+select customers_list.customer,
+	min(sale_date) as sale_date, -- получаем первую дату покупки
+	sellers_list.seller
+from sales
+inner join customers_list on customers_list.customer_id = sales.customer_id -- подтягиваем имя покупателя
+inner join sellers_list on sellers_list.employee_id = sales.sales_person_id -- подтягиваем имя продавца
+group by customer, seller -- группировка по покупателю и продавцу
+order by customer; -- сортировка по покупателю
+--------------------------------------------------------------------------------
+7 урок: 
